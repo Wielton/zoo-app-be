53c6101dd6b3b8a04fd4c90104a5babf42a0626d
Wielton <grantwielgosz@gmail.com>
2022-06-11 10:46:42 -0600

    Added app launch

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from db_helpers import run_query
+import sys
+
 app = Flask(__name__)
 
 @app.get('/api/animals')
@@ -18,3 +20,25 @@ def animals_post():
         return jsonify("Missing required argument 'imageURL'"), 422
     # TODO: Error checking the actual values for the arguments
     # TODO: DB write
+    
+
+
+
+
+
+if (len(sys.argv) > 1):
+    mode = sys.argv[1]
+else:
+    print("No mode argument: testing | production")
+    exit()    
+    
+if mode == "testing":
+    from flask_cors import CORS
+    CORS(app)    # Only want CORS on testing servers
+    app.run(debug=True)
+elif mode == "production":
+    import bjoern
+    bjoern.run(app, "0.0.0", 5000)
+    print('Running in development mode!')
+else:
+    print("Invalid mode.  Must be testing or production")
